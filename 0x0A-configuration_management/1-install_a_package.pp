26a489c811872cb1e190d695120c7c9efaee8024
Semhal22 <semhalgidena@gmail.com>
2024-03-23 14:53:10 +0000

    Installed werkzeug 2.1.1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,8 @@ exec {'install flask':
   command => 'pip3 install flask==2.1.0',
   path    => ['/usr/bin/'],
 }
+
+exec {'install werkzeug':
+  command => 'pip3 install werkzeug==2.1.1',
+  path    => ['/usr/bin']
+}
