3e8450cce67e79e8d2c86966b7163446d48014c6
Semhal Gidena <semhalgidena@gmail.com>
2024-04-01 19:26:54 +0000

    Add IdentitiesOnly

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # SSH client configuration must use private key and no password authentication
 file {'/etc/ssh/ssh_config':
     ensure  => 'file',
-    content => 'Identityfile ~/.ssh/school \n PasswordAuthentication no',
+    content => "Identityfile ~/.ssh/school\nIdentitiesOnly yes\nPasswordAuthentication no",
 }
